31898ca186895aa4365eae561f3adfa380223781
Keith Meehl <keith.meehl@gmail.com>
2017-10-09 18:33:56 -0400

    fix broken reset and stop argos commands

--- argos/time_tracker.0r.10s.sh
+++ argos/time_tracker.0r.10s.sh
@@ -7,6 +7,6 @@ echo "<span weight='normal' color='red'><tt>$tt_elapsed</tt></span> | iconName=a
 echo "---"
 
 echo "Timetracker(<span color='green'><tt>start</tt></span>) | iconName=appointment-new-symbolic bash=~/bin/time_tracker.sh terminal=false"
-echo "Timetracker(<span color='blue'><tt>context switch</tt></span>) | iconName=appointment-new-symbolic bash="touch ~/time_tracking/reset" terminal=false"
-echo "Timetracker(<span color='red'><tt>stop</tt></span>) | iconName=appointment-new-symbolic bash="touch ~/time_tracking/stop" terminal=false"
+echo "Timetracker(<span color='blue'><tt>context switch</tt></span>) | iconName=appointment-new-symbolic bash='touch ~/time_tracking/reset' terminal=false"
+echo "Timetracker(<span color='red'><tt>stop</tt></span>) | iconName=appointment-new-symbolic bash='touch ~/time_tracking/stop' terminal=false"
 echo "---"
